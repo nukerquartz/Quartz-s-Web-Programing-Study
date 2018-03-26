71511ab2399158295e00dcd592b97f0fc1447f58
nukerquartz <37802357+nukerquartz@users.noreply.github.com>
2018-03-26 21:34:27 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+ <meta charset="UTF-8">
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!--설정, 그 와 관련돤 코드?-->
+	<title>holololololololo</title>
+</head>
+
+<body>
+	<div>
+		<input  type="text" value="" placeholder="ID" style="display:block;" /><!--input 태그의 속성-->
+
+		<input type="password" value="" placeholder="Password" style="display:block;"/>
+
+		<button style="display:block;">로그인</button>
+
+		<a href="/signup.php" style="border-bottom:1px solid red; display: 
+		inline-block; font-size: 10px">회원가입</p>
+	</div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,11 @@
+ <meta charset="UTF-8">
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>sign up</title>
+ </head>
+ <body>
+ 	<p>Hello - Come in and sign up Here.</p>
+ </body>
+ </html>
